9acd3e3f6fa4f27d0c7658c83b942121613624e4
Yarin cohen <yarin.cohen999@gmail.com>
2022-07-17 20:47:25 +0300

    Updated Variables, function names

--- player.py
+++ player.py
@@ -1,41 +1,52 @@
 from mutagen.easyid3 import EasyID3
 import pygame
-from tkinter.filedialog import *
-from tkinter import *
+from tkinter.filedialog import askopenfilenames
+import tkinter as tk
 
-pygame.init()
-class MusicPlayer(Frame):
+SONG_NUM_PLAYS = 1
+SONG_START_TIME = 0.0
+
+class MusicPlayer(tk.Frame):
     def __init__(self,master):
         super(MusicPlayer, self).__init__(master)
-        #init variables
-        self.list1 = list()
-        self.pausing = False
-        self.list_index = 0
-        self.SONG_END = pygame.USEREVENT + 1
-        self.text1 = Text(self,wrap=WORD,width=60)
-        self.text1.grid(row=8,column=0)
-        self.label1 = Label(self, fg='Black',font=('Helvetica 12 bold italic',10),bg='ivory2')
-        self.label1.grid(row=6,column=0)
-
-        #init gui
         self.grid()
-        b1 = Button(self, text="PLAY SONG",command=self.button2,bg='AntiqueWhite1',width=40)
-        b1.grid(row=2,column=0)
-        b2 = Button(self, text="PREVIOUS SONG",command=self.button4,bg='AntiqueWhite1',width=40)
-        b2.grid(row=4,column=0)
-        b3 = Button(self, text="PAUSE/UNPAUSE",command=self.button3,bg='AntiqueWhite1',width=40)
-        b3.grid(row=3,column=0)
-        b4 = Button(self, text="NEXT SONG",command=self.button5,bg='AntiqueWhite1',width=40)
-        b4.grid(row=5,column=0)
-        b5 = Button(self, text="ADD TO LIST",command=self.button1,bg='AntiqueWhite1',width=40)
-        b5.grid(row=1,column=0)
+        #init variables
+        self.song_list = list()
+        self.pausing = False
+        self.current_song_index = 0
 
-    def songs_list_empty(self):
-        if not self.list1:
-            self.label1['text'] = "There are no songs in the list!"
+        self._ui_buttons_init()
+        self._ui_playlist_init()
+        self._ui_current_song_init()
+
+    def _ui_current_song_init(self):
+        self.status = tk.Label(self, fg='Black',font=('Helvetica 12 bold italic',10),bg='ivory2')
+        self.status.grid(row=6,column=0)
+
+    def _ui_playlist_init(self):
+        self.playlist = tk.Text(self,wrap=tk.WORD,width=60)
+        self.playlist.grid(row=8,column=0)
+
+    def _ui_buttons_init(self):
+        play_button = tk.Button(self, text="PLAY SONG",command=self._play_song,bg='AntiqueWhite1',width=40)
+        play_button.grid(row=2,column=0)
+        previous_button = tk.Button(self, text="PREVIOUS SONG",command=self._play_previous_song,bg='AntiqueWhite1',width=40)
+        previous_button.grid(row=4,column=0)
+        pause_button = tk.Button(self, text="PAUSE/UNPAUSE",command=self._pause_song,bg='AntiqueWhite1',width=40)
+        pause_button.grid(row=3,column=0)
+        next_button = tk.Button(self, text="NEXT SONG",command=self._play_next_song,bg='AntiqueWhite1',width=40)
+        next_button.grid(row=5,column=0)
+        add_button = tk.Button(self, text="ADD TO LIST",command=self._add_song_to_queue,bg='AntiqueWhite1',width=40)
+        add_button.grid(row=1,column=0)
+
+    def _songs_list_empty(self):
+
+        if not self.song_list:
+            self.status['text'] = "There are no songs in the list!"
             return True
         return False
-    def check_song_data(self,item):
+
+    def _get_song_data(self,item):
         song = EasyID3(item)
 
         if song.get('title') is not None and song.get('artist') is not None:
@@ -43,39 +54,39 @@ class MusicPlayer(Frame):
         else:
             return item
 
-    def button1(self):
+    def _add_song_to_queue(self):
         directory = askopenfilenames()
+
         for song_dir in directory:
             print(song_dir)
-            self.list1.append(song_dir)
-        self.text1.delete(0.0, END)
+            self.song_list.append(song_dir)
+        self.playlist.delete(0.0, tk.END)
 
-        for key, item in enumerate(self.list1):
-            song_data = str(key + 1) + '. ' + self.check_song_data(item)
-            self.text1.insert(END, song_data + '\n')
+        for key, item in enumerate(self.song_list):
+            song_data = str(key + 1) + '. ' + self._get_song_data(item)
+            self.playlist.insert(tk.END, song_data + '\n')
     #################################################################################
-    def song_data(self):
-        song_data = "Now playing song number: " + str(self.list_index + 1) + " " + self.check_song_data(self.list1[self.list_index])
-        return song_data
-    #################################################################################
-    def button2(self):
-        if self.songs_list_empty():
+    def _play_song(self):
+
+        if self._songs_list_empty():
             return
-        directory = self.list1[self.list_index]
+        directory = self.song_list[self.current_song_index]
 
         pygame.mixer.music.load(directory)
-        pygame.mixer.music.play(1, 0.0)
-        pygame.mixer.music.set_endevent(self.SONG_END)
+        pygame.mixer.music.play(SONG_NUM_PLAYS, SONG_START_TIME)
+        pygame.mixer.music.set_endevent(pygame.USEREVENT + 1)
         self.pausing = False
-        self.label1['text'] = self.song_data()
+        self.status['text'] = "Now playing song number: " + str(self.current_song_index + 1) + " " + self._get_song_data(self.song_list[self.current_song_index])
     #################################################################################
-    def check_music(self):
+    def _check_song_ended(self):
+
         for event in pygame.event.get():
-            if event.type == self.SONG_END:
-                self.button5()
+            if event.type == pygame.mixer.music.get_endevent():
+                self._play_next_song()
     #################################################################################
-    def button3(self):
-        if self.songs_list_empty():
+    def _pause_song(self):
+
+        if self._songs_list_empty():
             return
 
         if self.pausing:
@@ -85,35 +96,52 @@ class MusicPlayer(Frame):
             pygame.mixer.music.pause()
             self.pausing = True
     #################################################################################
-    def get_next_song(self):
-        if self.list_index + 2 <= len(self.list1):
-            return self.list_index + 1
+    def _get_next_song(self):
+
+        if self.current_song_index + 2 <= len(self.song_list):
+            return self.current_song_index + 1
         else:
             return 0
     #################################################################################
-    def button5(self):
-        if self.songs_list_empty():
+    def _play_next_song(self):
+
+        if self._songs_list_empty():
             return
 
-        self.list_index = self.get_next_song()
-        self.button2()
+        self.current_song_index = self._get_next_song()
+        self._play_song()
     #################################################################################
-    def get_previous_song(self):
-        if self.list_index - 1 >= 0:
-            return self.list_index - 1
+    def _get_previous_song(self):
+
+        if self.current_song_index - 1 >= 0:
+            return self.current_song_index - 1
         else:
-            return len(self.list1) - 1
+            return len(self.song_list) - 1
     #################################################################################
-    def button4(self):
-        if self.songs_list_empty():
+    def _play_previous_song(self):
+
+        if self._songs_list_empty():
             return
-        self.list_index = self.get_previous_song()
-        self.button2()
+        self.current_song_index = self._get_previous_song()
+        self._play_song()
 
-window = Tk()
-window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(0, weight=1)
-window.title("MP3 Music Player")
-app = MusicPlayer(window)
 
-window.mainloop()
+def close_app():
+    return True
+
+def main():
+    pygame.init()
+    window = tk.Tk()
+    window.grid_rowconfigure(0, weight=1)
+    window.grid_columnconfigure(0, weight=1)
+    window.title("MP3 Music Player")
+    app = MusicPlayer(window)
+
+    #tk.mainloop()
+
+    while True:
+        app._check_song_ended()
+        app.update()
+
+if __name__ == "__main__":
+    main()
