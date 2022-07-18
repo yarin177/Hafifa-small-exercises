bfb99d3a51e69ac9aa64f9ecf97effd5ecf9abc1
Yarin cohen <yarin.cohen999@gmail.com>
2022-07-18 15:48:52 +0300

    Added new function
    1. A function to remove old files and dir in the output folder
    2. A function to create a new dir for every url
    3. A function to retrieve HTML from a url

--- ex2-web-scrapping/browser.py
+++ ex2-web-scrapping/browser.py
@@ -1,13 +1,36 @@
 import os
+import json
+import requests
+
+def delete_output_dir():
+    for url_folder in os.listdir('output'):
+        local_folder_path = os.path.join('output', url_folder)
+        for file in os.listdir(local_folder_path):
+            os.remove(os.path.join(local_folder_path, file))
+        os.rmdir(local_folder_path)
+
+def create_url_folders(urls: list):
+    for i in range(len(urls)):
+        os.mkdir('output//' + str(i+1) +  '_url')
+
+def get_html(url: str) -> str:
+    r = requests.get(url)
+
+    if r.status_code == 200:
+        return r.text
+    else:
+        return 'Could not retrieve HTML'
 
 def main():
     if not os.path.isdir("output"):
         os.mkdir("output")
+    else:
+        delete_output_dir()  
 
+    
     urls = open('input//urls.input','r').read().split('\n')
-    for i in range(len(urls)):
-        os.mkdir('output//' + str(i+1) +  '_url')
-
+    create_url_folders(urls)
+    get_html(urls[0])
 
 if __name__ == "__main__":
     main()
