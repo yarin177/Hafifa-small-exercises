ec0ea308ba6bf8e8ac72e19f477434973e50ac60
Yarin cohen <yarin.cohen999@gmail.com>
2022-07-18 14:46:15 +0300

    Added more needed files

--- ex2-web-scrapping/browser.py
+++ ex2-web-scrapping/browser.py
@@ -1,1 +1,13 @@
-#Cool code
+import os
+
+def main():
+    if not os.path.isdir("output"):
+        os.mkdir("output")
+
+    urls = open('input//urls.input','r').read().split('\n')
+    for i in range(len(urls)):
+        os.mkdir('output//' + str(i+1) +  '_url')
+
+
+if __name__ == "__main__":
+    main()
