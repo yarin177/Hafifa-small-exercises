b3ed8aadbf2bc23880f179d91d7b33dc52e66e31
Yarin cohen <yarin.cohen999@gmail.com>
2022-07-19 13:01:17 +0300

    Added few more functions
    1. A function to get external urls
    2. A function to get external image urls
    Moved code from main to functions

--- ex2-web-scrapping/browser.py
+++ ex2-web-scrapping/browser.py
@@ -1,36 +1,48 @@
 import os
 import json
-import requests
+from django.urls import path
+from selenium import webdriver
+
+INPUT_FOLDER = 'input' 
+INPUT_FILE = os.path.join(INPUT_FOLDER, 'urls.input').replace("\\","/")
+OUTPUT_FOLDER = 'output'
 
 def delete_output_dir():
-    for url_folder in os.listdir('output'):
-        local_folder_path = os.path.join('output', url_folder)
+    for url_folder in os.listdir(OUTPUT_FOLDER):
+        local_folder_path = os.path.join(OUTPUT_FOLDER, url_folder)
         for file in os.listdir(local_folder_path):
             os.remove(os.path.join(local_folder_path, file))
         os.rmdir(local_folder_path)
 
 def create_url_folders(urls: list):
     for i in range(len(urls)):
-        os.mkdir('output//' + str(i+1) +  '_url')
+        os.mkdir(os.path.join(OUTPUT_FOLDER, f"{str(i+1)}_url"))
 
-def get_html(url: str) -> str:
-    r = requests.get(url)
+def get_html(browser: webdriver.Chrome) -> str:
+    return browser.page_source
 
-    if r.status_code == 200:
-        return r.text
+def get_external_links(browser: webdriver.Chrome) -> list:
+    imageLinks = [image.get_attribute("src") for image in browser.find_elements_by_xpath("//a//img")]
+    website_urls = [elem.get_attribute("href") for elem in browser.find_elements_by_xpath("//a[@href]")]
+    return [imageLinks + website_urls]
+
+def init_output_folder():
+    if not os.path.isdir(OUTPUT_FOLDER):
+        os.mkdir(OUTPUT_FOLDER)
     else:
-        return 'Could not retrieve HTML'
+        delete_output_dir() 
 
-def main():
-    if not os.path.isdir("output"):
-        os.mkdir("output")
-    else:
-        delete_output_dir()  
+def get_urls_from_file() -> list:
+    return open(INPUT_FILE,'r').read().split('\n')
 
-    
-    urls = open('input//urls.input','r').read().split('\n')
+def main():  
+    urls = get_urls_from_file()
+    init_output_folder()
     create_url_folders(urls)
-    get_html(urls[0])
+    
+    #browser = webdriver.Chrome()
+    #browser.get('http://selenium.dev/')
+    #browser.save_screenshot('screenshot.png')
 
 if __name__ == "__main__":
     main()
