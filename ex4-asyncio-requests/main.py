39ba02410f4d85af010886438d0c712e8e61187c
Yarin cohen <yarin.cohen999@gmail.com>
2022-07-26 16:06:39 +0300

    Added comments and spaces

--- ex4-asyncio-requests/main.py
+++ ex4-asyncio-requests/main.py
@@ -6,10 +6,16 @@ FILE_PATH = 'lines.txt'
 SERVER_URL_8080 = 'http://localhost:8080'
 SERVER_URL_8081 = 'http://localhost:8081'
 
-def get_lines_from_file():
-    return open(FILE_PATH,'r').read().split('\n')
-
-async def fetch(word,session,session2):
+async def fetch(word: str,session: aiohttp.ClientSession(),session2: aiohttp.ClientSession()) -> bool:
+    """This function sends a word(string) to both servers via HTTP GET requests,
+        compares the results and returns True/False accordingly.
+    Args:
+        word: (str) A word from the local FILE_PATH file.
+        session: a ClientSession for SERVER_URL_8080
+        session2: a ClientSession for SERVER_URL_8081
+    Returns:
+        True if both servers returned the same word, False otherwise.
+    """
     async with session.get(f"{SERVER_URL_8080}/{word}") as response:
         res1 = await response.text()
 
@@ -20,13 +26,27 @@ async def fetch(word,session,session2):
         return True
     return False
 
-async def gather_coroutines(words):
+async def gather_coroutines(words: list) -> asyncio.Future:
+    """This function creates a coroutine for each word in words,
+        each coroutine running the fetch() function with a word.
+    Args:
+        words: (list) A list of words(strings)
+    Returns:
+        Future instance
+    """
     async with aiohttp.ClientSession() as session, aiohttp.ClientSession() as session2:
         return await asyncio.gather(*[
             fetch(word,session,session2) for word in words
         ])
 
-def request_servers(words):
+def request_servers(words: list) -> list:
+    """This function creates and runs coroutines,
+        Eventually returns the results for all coroutines
+    Args:
+        words: (list) A list of words(strings)
+    Returns:
+        results
+    """
     tasks = gather_coroutines(words)
     loop = asyncio.get_event_loop()
     results = loop.run_until_complete(tasks)
@@ -34,17 +54,17 @@ def request_servers(words):
     
     return results
 
-def write_output(results):
-    with open('output.txt', 'w') as fp:
-        fp.write("\n".join(str(result) for result in results))
-
-
 def main():
-    words = get_lines_from_file()
     start = timeit.default_timer()
-    results = request_servers(words)
-    write_output(results)
-    stop = timeit.default_timer()
-    print('Time: ', stop - start) 
+    # read all words from local file
+    with open(FILE_PATH, 'r', encoding='utf-8') as f:
+        words = f.read().split('\n')
+    results = request_servers(words[0:10000])
+    # write results to local file
+    with open('output.txt', 'w', encoding='utf-8') as fp:
+        fp.write("\n".join(str(result) for result in results))
+    print('Time: ', timeit.default_timer() - start) 
 
-main()
+
+if __name__ == '__main__':
+    main()
--- ex4-asyncio-requests/server.py
+++ ex4-asyncio-requests/server.py
@@ -14,12 +14,12 @@ def get_port_from_parser():
     return 8080
 
 @app.route('/<str>', methods = ['GET'])
-def get_random_string(str):
+def get_random_string(string):
     rand_num = randint(1,3)
     if rand_num == 1:
-        return str
-    else:
-        return ''
+        return string
+    return ''
+
 
 if __name__ == '__main__':
     port = get_port_from_parser() # default is 8080
